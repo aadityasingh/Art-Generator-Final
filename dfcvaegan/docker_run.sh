567a5197aea2ed1cab74fbfb5b3e08edcc902b6f
Aaditya Singh <aaditya@mit.edu>
2019-02-18 00:56:09 -0500

    More docker stuff.

--- dfcvaegan/docker_run.sh
+++ dfcvaegan/docker_run.sh
@@ -2,7 +2,7 @@
 
 # Set up mounts & pass environment and commandline through
 nvidia-docker run \
-    -v /data:/data \
+    -v $HOME/Art-Generator-Final/dfcvaegan:/dfcvaegan \
     -e CUDA_VISIBLE_DEVICES \
     aaditya \
     $*
