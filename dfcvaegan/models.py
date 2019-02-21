74452fce82020bb85a1f337238fdd6e8da8fa176
eric-qian-d <eric.qian.d@gmail.com>
2019-02-20 21:06:38 -0500

    changed discriminator model to actually have ce

--- dfcvaegan/models.py
+++ dfcvaegan/models.py
@@ -115,14 +115,15 @@ class DCDiscriminator(nn.Module):
         self.conv3 = conv(in_channels=conv_dim * 2, out_channels=conv_dim * 4, kernel_size=4)
         self.conv4 = conv(in_channels=conv_dim * 4, out_channels=1, kernel_size=4, stride=1, padding=0,
                           batch_norm=False)
+        self.linear = linear(conv_dim * 4 * 4 * 4, 1)
 
     def forward(self, x):
 
         out = F.relu(self.conv1(x))    # BS x 64 x 16 x 16
         out = F.relu(self.conv2(out))  # BS x 128 x 8 x 8
         out = F.relu(self.conv3(out))  # BS x 256 x 4 x 4
-
-        out = self.conv4(out).squeeze()
+        out = F.relu(self.conv4(out))
+        out = self.linear(out)
         out = F.sigmoid(out)
         print(out)
         return out
